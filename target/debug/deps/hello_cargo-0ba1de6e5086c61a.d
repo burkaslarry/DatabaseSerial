4f12af84ba7d19b5f15c9b544f8ccaf3aa70fc1f
Larry Lo <larrylo@capax-tech.com>
2023-12-23 18:34:43 +0800

    change to rust projet

---
+++ target/debug/deps/hello_cargo-0ba1de6e5086c61a.d
@@ -0,0 +1,5 @@
+/Users/larrylo/DatabaseSerial/hello_cargo/target/debug/deps/hello_cargo-0ba1de6e5086c61a: src/main.rs
+
+/Users/larrylo/DatabaseSerial/hello_cargo/target/debug/deps/hello_cargo-0ba1de6e5086c61a.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/larrylo/DatabaseSerial/target/debug/hello_cargo: /Users/larrylo/DatabaseSerial/src/main.rs
